a6600735c57828d7365d894cec45f8fc68384b32
chenyinjie0810 <cyj7416146>
2020-07-26 23:06:04 +0800

    数据校验增删盖查

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -18,6 +18,7 @@
             size="mini"
             @click="() => append(data)"
           >Append</el-button>
+          <el-button v-if="node.level <=2" type="text" size="mini" @click="() => edit(data)">Edit</el-button>
           <el-button
             v-if="node.childNodes.length==0"
             type="text"
@@ -27,6 +28,24 @@
         </span>
       </span>
     </el-tree>
+    <!-- 表单 -->
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :close-on-click-modal="false">
+      <el-form ref="form" :model="category" label-width="80px">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name"></el-input>
+        </el-form-item>
+        <el-form-item label="排序号">
+          <el-input v-model="category.sort"></el-input>
+        </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submit">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -36,8 +55,19 @@ export default {
   props: {},
   data() {
     return {
+      dialogType: "",
+      category: {
+        name: null,
+        parentCid: null,
+        catLevel: null,
+        showStatus: null,
+        sort: null,
+        productCount: null,
+        icon: null
+      },
+      dialogVisible: false,
       data: [],
-      expandIds:[],
+      expandIds: [],
       defaultProps: {
         children: "children",
         label: "name",
@@ -57,9 +87,82 @@ export default {
         this.data = data.tree;
       });
     },
+    submit() {
+      debugger
+      if (this.dialogType == "add") {
+        this.addCategory();
+      } else if (this.dialogType == "upd") {
+        this.editCategory();
+      }
+    },
     // 提交方法
     append(data) {
+      this.category.catId = null;
+      this.category.name = null;
+      this.category.sort = null;
+      this.category.icon = null;
+      this.category.sort = null;
+      this.dialogType = "add";
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
       console.log(data);
+    },
+    //添加分类
+    addCategory() {
+      console.log("提交的三级分类," + this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "菜单添加成功",
+          type: "success",
+        });
+        this.dialogVisible = false;
+        this.getAllMenus();
+        this.expandIds = [this.category.parentCid];
+      });
+    },
+    //打开修改分类页
+    edit(data) {
+      this.dialogType = "upd";
+      var catId = data.catId;
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/${catId}`),
+        method: "get",
+      }).then(({ data }) => {
+        if (data.code == 0) {
+          this.dialogVisible = true;
+          this.category.catId = data.category.catId;
+          this.category.name = data.category.name;
+          this.category.parentCid = data.category.parentCid;
+          this.category.catLevel = data.category.catLevel;
+          this.category.sort = data.category.sort;
+          this.category.icon = data.category.icon;
+          this.category.sort = data.category.sort;
+        }
+      });
+    },
+    //提交修改
+    editCategory() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then(({ data }) => {
+        console.log(data);
+        if (data.code == 0) {
+           this.$message({
+              message: "菜单修改成功",
+              type: "success",
+            });
+            this.dialogVisible = false;
+            this.getAllMenus();
+            this.expandIds = [data.category.catId];
+        }
+      });
     },
     // 删除方法
     remove(node, data) {
@@ -80,7 +183,7 @@ export default {
               type: "success",
             });
             this.getAllMenus();
-            this.expandIds=[node.parent.data.catId]
+            this.expandIds = [node.parent.data.catId];
           });
         })
         .catch(() => {});
